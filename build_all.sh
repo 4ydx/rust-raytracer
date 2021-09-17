ebc39d53eeecf716938636f6e43a03afe16099be
Nathan <nathan@4ydx.com>
2021-09-17 18:02:17 +0900

    adding all images

--- build_all.sh
+++ build_all.sh
@@ -2,15 +2,17 @@ set -e
 
 cd raylib; cargo build; cd ..
 
-for i in 03.3 04.2 05.2 06.1 06.2 06.7; do
+for i in 03.3 04.2 05.2 06.1 06.2 06.7 07.2 08.2 08.3 08.4 08.5 08.6 09.6 09.6 10.2 10.3 10.4 10.5 11.1 11.2 12.2; do
   echo "=============================================";
   echo $i;
   cd $i; cargo build --release;
-  rm example.ppm; ./target/debug/raytracer; cd ..
+  rm example.ppm;
+  { time ./target/release/raytracer; } 2> time.log
+  cd ..
 done
 
-# to slow to actually run the raytracer as a group
-for i in 07.2 08.2 08.3 08.4 08.5 08.6 09.5 09.6 10.2 10.3 10.4 10.5 11.1 11.2 12.2 13.1; do
+# too slow (without threading is ~45 minutes)
+for i in 13.1; do
   echo "=============================================";
   echo $i;
   cd $i; cargo build --release; cd ..
